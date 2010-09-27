b2b1f1b29a0a78f019edc02429ac7a068811f8c5
Matt Forbes <matt@grablisting.com>
2010-09-27 01:17:26 -0700

    finished order page...working on checkout

--- acm/cabinet/views.py
+++ acm/cabinet/views.py
@@ -1,7 +1,9 @@
 from django.core.context_processors import csrf
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response
 from acm.cabinet.models import *
+from decimal import *
+import simplejson
 
 def order(request):
 	items = Item.objects.all()
@@ -14,4 +16,16 @@ def suggest(request):
 	return render_to_response('cabinet/suggest.html', {'message': 'matt'});
 
 def checkout(request):
-	return HttpResponse("HEY");
+	if request.method == "POST" and "data" in request.POST and "wnumber" in request.POST:
+		c = {}
+		wnumber = request.POST['wnumber']
+		print wnumber
+		try:
+			c['profile'] = UserProfile.objects.get(user__username=wnumber)
+		except UserProfile.DoesNotExist:
+			u, created = User.objects.get_or_create(username=wnumber)
+			c['profile'] = UserProfile.objects.create(user=u, credit=Decimal('0.00'))
+		c['data'] = simplejson.loads(request.POST['data'])
+		return render_to_response('cabinet/checkout.html', c)
+	else:
+		raise Http404
--- acm/media/js/order.js
+++ acm/media/js/order.js
@@ -1,46 +1,70 @@
 window.onload = function() {
-	var cart = { total:0.0 },
+	var cart = {},
 		items = document.getElementById("items").children,
 		p = /(.+) \(\$([\d.]+)\) ID#(\d+)/,
-		item,m;
+		item,m,total=0.0;
 	for(var i=0, j=items.length; i < j; i++) {
 		item = items[i];
 		m = item.children[0].innerText.match(p);
 		item.onclick=(function(name, price, id) {
 			return function() {
-				cart[id] ? cart[id] = [name, cart[id][1]+1]
-						 : cart[id] = [name, 1];
-				cart.total += parseFloat(price);
-				updateCartDiv(cart);
+				cart[id] ? cart[id] = { name: name, quantity: cart[id].quantity+1 }
+						 : cart[id] = { name: name, quantity: 1 };
+				total += parseFloat(price);
+				updateCartDiv(cart, total);
 			}
 		})(m[1], m[2], m[3]);
 	}
 	document.getElementById("purchase").onclick = function() {
 		var form = document.getElementById("purchase_form"),
-			data = document.createElement("input");
-		form.setAttribute("action", "checkout");
-		form.setAttribute("method", "POST");
-		data.setAttribute("type", "hidden");
-		data.setAttribute("name", "data");
-		data.setAttribute("value", JSON.stringify(cart));
-		//form.appendChild(data);
-		console.log(JSON.stringify(cart));
+			data = document.createElement("input"),
+			wnumber = document.getElementById("wnumber").value,
+			error = document.getElementById("wnumber_error"),
+			p = /\d{8}/;
+
+		// little validation
+		if(!p.test(wnumber)) {
+			error.innerText = "WNumber required";
+			return;
+		} else if (empty(cart)) {
+			error.innerText = "Empty cart";
+			return;
+		}
+
+		// add items and go
+		addKeyVal(form, "data", JSON.stringify(cart));
+		addKeyVal(form, "wnumber", wnumber); 
 		form.submit();
 	}
 }
 
-function updateCartDiv(cart) {
+function updateCartDiv(cart, total) {
 	var d = document.createElement("div"),
 		cartDiv = document.getElementById("cart"),
 		elem, total=0;
 	for(i in cart) {
 		if (i != "total") {
 			elem = document.createElement("div");
-			elem.innerText = cart[i][1] + " x " + cart[i][0];
+			elem.innerText = cart[i].quantity + " x " + cart[i].name;
 			d.appendChild(elem);
 		}
 	}
 	d.setAttribute("class", "cart_items");
 	cartDiv.replaceChild(d, cartDiv.children[1]);
-	document.getElementById("cart_total").innerText = cart.total.toFixed(2);
+	document.getElementById("cart_total").innerText = total.toFixed(2);
+}
+
+function addKeyVal(form, key, val) {
+	var data = document.createElement("input");
+	data.setAttribute("type", "hidden");
+	data.setAttribute("name", key);
+	data.setAttribute("value", val);
+	form.appendChild(data);
+}
+
+function empty(obj) {
+	for(i in obj) {
+		return false;
+	}
+	return true;
 }
